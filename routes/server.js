03519237bdcce7f26a45c54b9e827eb5970e1d95
Dmitriy-Belskiy <74544373+Dmitriy-Belskiy@users.noreply.github.com>
2021-11-30 13:06:36 +0300

    Получили товары из CSV файла

--- routes/server.js
+++ routes/server.js
@@ -1,8 +1,24 @@
+
+
 const router = require("express").Router();
+const fs = require("fs")
+
+let goodsNames = ["Название", "Категория", "Артикул", "Цвет", "Размер", "Кол-во", "Цена", "Бренд"];
+let filetext = fs.readFileSync("./data/goods.csv", "utf-8");
+// console.log(filetext);
+let goods = filetext.split("\n");
+
+for(let i = 0; i < goods.length; i++){
+    goods[i] = goods[i].split(";");
+}
+// console.log(goods);
 
 router.get("/", function(require, response){
     // response.send("<h1>Магазин одежды</h1>");
-    response.render("index")
+    response.render("index", {
+        names: goodsNames,
+        goods: goods
+    })
 })
 
 module.exports = router;
