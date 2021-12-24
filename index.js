cb30b078ab898a8374542511b112408d0b8055ad
Dmitriy-Belskiy <74544373+Dmitriy-Belskiy@users.noreply.github.com>
2021-12-24 14:18:32 +0300

    Сделали админку

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = require("./routes/server.js");
+const admin = require("./routes/admin");
 const PORT = process.env.PORT || 8000;
+
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -8,5 +10,6 @@ app.set("views", "./views");
 app.use(express.json());
 app.use(express.static("./public")); 
 app.use("/", server);
+app.use("/admin", admin);
 
 app.listen(PORT);
